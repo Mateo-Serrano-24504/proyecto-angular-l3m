d9f4c86336a12fa64f438e4cea69724b3de93e5b
Matias Dworzak <medworzak@gylgroup.com>
2026-05-22 11:30:26 -0300

    cambios para merge

--- back/src/main/java/backend/Dominio/puertos/in/puntaje/ActualizarPuntaje.java
+++ back/src/main/java/backend/Dominio/puertos/in/puntaje/ActualizarPuntaje.java
@@ -1,9 +1,9 @@
 package backend.Dominio.puertos.in.puntaje;
 
+import backend.Aplicacion.dto.puntaje.RegistrarPuntajeDTORequest;
 import backend.Dominio.modelo.PuntajeModel;
 
 public interface ActualizarPuntaje {
-    PuntajeModel ejecutar(Long id, ActualizarPuntajeDTORequest request);
+    PuntajeModel ejecutar(Long id, RegistrarPuntajeDTORequest request);
 
 }
-
