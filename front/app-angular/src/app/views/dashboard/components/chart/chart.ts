93b390d4de594d416dd4a52d5296056719340964
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-05-26 12:05:16 -0300

    chore/Elimino el uso de ChangeDetectorRef

--- front/app-angular/src/app/views/dashboard/components/chart/chart.ts
+++ front/app-angular/src/app/views/dashboard/components/chart/chart.ts
@@ -1,5 +1,5 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, inject, signal } from '@angular/core';
 import { ChartModule } from 'primeng/chart';
 import { EstudiantesService } from '../../../../services/estudiantes.service';
 import { ChangeDetectorRef } from '@angular/core';
@@ -11,60 +11,66 @@ import { ChangeDetectorRef } from '@angular/core';
   imports: [ChartModule],
 })
 export class ChartRadar implements OnInit {
-  data: any;
+  data = signal<any>({});
   options: any;
   platformId = inject(PLATFORM_ID);
 
   constructor(
-    private studentService: EstudiantesService,
-    private cd: ChangeDetectorRef,
+    private studentService: EstudiantesService
   ) {}
 
   ngOnInit() {
+    this.configureOptions();
     this.initChart();
+  }
+
+  configureOptions() {
+    this.options = {
+      plugins: {
+        legend: {
+          labels: {
+            color: '#ffffff',
+          },
+        },
+      },
+      scales: {
+        r: {
+          min: 1,
+          max: 10,
+          ticks: {
+            stepSize: 1,
+          },
+          grid: {
+            color: 'rgba(255,255,255,0.2)',
+          },
+          angleLines: {
+            color: 'rgba(255,255,255,0.2)',
+          },
+          pointLabels: {
+            color: '#e5e7eb',
+          },
+        },
+      },
+    };
   }
 
   initChart() {
     if (isPlatformBrowser(this.platformId)) {
       this.studentService.getChart().subscribe((res) => {
-        this.data = {
-          labels: res.labels,
-          datasets: [
-            {
-              label: 'Rendimiento',
-              borderColor: '#22c55e',
-              backgroundColor: 'rgba(34, 197, 94, 0.2)',
-              data: res.data,
-            },
-          ],
-        };
-
-        this.cd.detectChanges();
+        this.data.set(
+          {
+            labels: res.labels,
+            datasets: [
+              {
+                label: 'Rendimiento',
+                borderColor: '#22c55e',
+                backgroundColor: 'rgba(34, 197, 94, 0.2)',
+                data: res.data,
+              },
+            ],
+          }
+        );
       });
-
-     
-      this.options = {
-        plugins: {
-          legend: {
-            labels: {
-              color: '#ffffff',
-            },
-          },
-        },
-        scales: {
-          r: {
-            grid: {
-              color: 'rgba(255,255,255,0.2)',
-            },
-            angleLines: {
-              color: 'rgba(255,255,255,0.2)',
-            },
-            pointLabels: {
-              color: '#e5e7eb',
-            },
-          },
-        },
-      };
     }
   }
 }
